4b8b5e9296493914dc7293b59123763a9aaaf4f8
QQmimo <qqmimo@yandex.ru>
2025-04-05 00:58:37 +0500

    Добавил проверку diff для объектов и массивов

--- src/Applications/Utilities/Utilities.ts
+++ src/Applications/Utilities/Utilities.ts
@@ -1,12 +1,29 @@
 export class Utilities {
     public static getDiff<T>(origin: T, edited: T): Partial<T> | null {
         const diff: Partial<T> = {};
-        const keys: string[] = Object.keys(origin as { new(): T; });
+        const keys: (keyof T)[] = Object.keys(origin as { new(): T; }) as (keyof T)[];
         let foundedDiff: boolean = false;
         keys.forEach(key => {
-            if (origin[key as keyof T] !== edited[key as keyof T]) {
+            if (typeof origin[key] === 'object') {
+                if ((origin[key] as Array<unknown>)?.length !== undefined
+                    || (edited[key] as Array<unknown>)?.length !== undefined) {
+                    const arrayDiff: Partial<typeof key> | null = this.getDiff<typeof key>(origin[key] as typeof key, edited[key] as typeof key);
+                    if ((origin[key] as Array<unknown>)?.length !== (edited[key] as Array<unknown>)?.length
+                        || arrayDiff !== null) {
+                        foundedDiff = true;
+                        diff[key] = edited[key];
+                    }
+                } else {
+                    const objectDiff: Partial<typeof key> | null = this.getDiff<typeof key>(origin[key] as typeof key, edited[key] as typeof key);
+                    if (objectDiff !== null) {
+                        foundedDiff = true;
+                        diff[key] = edited[key];
+                    }
+                }
+            }
+            else if (origin[key] !== edited[key]) {
                 foundedDiff = true;
-                diff[key as keyof T] = edited[key as keyof T];
+                diff[key] = edited[key];
             }
         });
 
